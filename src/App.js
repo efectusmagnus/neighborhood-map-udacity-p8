520856c8bf6fbd3a7c33ee5bf06194549a6633ff
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-12-10 04:41:57 +0100

    Test Map without Map package (not working)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 //import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    // this.getVenues()
+    this.renderMap()
+  }
   // The `renderMap` loads the script
   renderMap = () => {
     loadScript("htpps://maps.googleapis.com/maps/api/js?key=AIzaSyBnR461-IpsYquMTh4jytwZceXMQrbHVeY&callback=initMap")
+    window.initMap = this.initMap
   }
+/*
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore"
+    const parameters = {
+      client_id: "",
+      client_secret: "",
+      query: "food",
+      near: "Sydney"
+    }
+    axios.get(endPoint + new URLSearchParams(parameters))
+     .then(response => {
+       console.log(response)
+     })
+     .catch(error => {
+       console.log("ERROR! " + error)
+     })
+  }*/
   initMap = () => {
     // Define google though the window
     const map = new window.google.maps.Map(document.getElementById('map'), {
@@ -24,7 +47,6 @@ class App extends Component {
   }
 }
 // Use the loadScript function by passing a parameter to it
-
 /** The `script tag to be integrated to react`
 Create a function called loadScript so that we can write a function using the
 `function` keyword without any problems.
@@ -35,11 +57,13 @@ The below function is the same as:
   async defer></script>`
  We are accessing the properties of our tags and gives them another value
 */
+
 function loadScript(url) {
   // Select the first tag
-  const index = window.document.getElementByTagName('script')[0]
+  var index = window.document.getElementsByTagName("script")[0]
   // Create the script tag
-  const script = window.document.createElement('script')
+  var script = window.document.createElement("script")
+  // script.type = "text/javascript"
   script.src = url
   script.async = true
   script.defer = true
@@ -47,5 +71,9 @@ function loadScript(url) {
   // the reference or the first script tag(`script: newNode` and `index: referenceNode`).
   index.parentNode.insertBefore(script, index)
 }
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 
 export default App;

aeb4b8971e941fa28a46e6c80657bea38fb93c89
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-12-19 19:16:18 +0100

    Add a Footer as Component rather than in html.

---
+++ src/js/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+  render() {
+    return(
+      <footer role="contentinfo">
+        <p>Copyright (c) 2018 <a href="/" aria-label="Home Page">
+            <strong>Musea Maps Finder</strong>
+          </a> All Rights Reserved.
+        </p>
+      </footer>
+    );
+  }
+}

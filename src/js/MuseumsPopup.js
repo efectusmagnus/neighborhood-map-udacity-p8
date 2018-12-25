cf7665bb6e1be7208cc00aa56406647d17d8b0e4
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-12-25 23:40:38 +0100

    Info window does not functions with this packet

--- src/js/MuseumsPopup.js
+++ src/js/MuseumsPopup.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
-import { InfoWindow} from 'react-google-maps';
+import React from 'react';
+import { InfoWindow } from 'react-google-maps';
 
 function MuseumsPopup(props) {
-  let marker = props.marker
-  let image = marker.categories[0].icon.prefix
+  let marker = props.marker;
+  let image = marker.categories[0].icon.prefix;
   return(
-    <InfoWindow>
+    <InfoWindow
+      //marker={this.state.activeMarker}
+      //visible={this.state.showingInfoWindow}
+      key={ marker.name }
+      className="info-window"
+      position={{lat: marker.location.lat, lng: marker.location.lng}}
+    >
       <h3>{marker.name}</h3>
       <p>{marker.location.address}</p>
       <p>{marker.location.postalCode}</p>
       <p className="credits">Credits to
       <a href="https://foursquare.com"> Foursquare</a></p>
-      <img alt={marker.name} src={(marker.categories[0].icon.prefix.prefix) + (marker.id)+(marker.categories[0].icon.sufix)} />
     </InfoWindow>
   )
 }

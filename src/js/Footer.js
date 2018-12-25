6b8447d4659a24b557ede93afa007b39c8c9af82
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-12-25 23:33:36 +0100

    This structure allows spacing (!important)

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -4,10 +4,9 @@ export default class Footer extends Component {
   render() {
     return(
       <footer role="contentinfo">
-        <p>Copyright (c) 2018 <a href="/" aria-label="Home Page">
-            <strong>Musea Maps Finder</strong>
-          </a> All Rights Reserved.
-        </p>
+        <p tabIndex="0">Copyright (c) 2018. <a href="/" aria-label="Home Page">
+            <strong>Musea Maps Finder. </strong>
+          </a> All Rights Reserved.</p>
       </footer>
     );
   }

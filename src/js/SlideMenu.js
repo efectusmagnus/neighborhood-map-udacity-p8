979b9fd7148743e5ba15816054e08ab04f80d5d5
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-12-25 23:41:37 +0100

    Test if the click functions (SlideMenu.js)

--- src/js/SlideMenu.js
+++ src/js/SlideMenu.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 
 class SlideMenu extends Component {
+  constructor(props) {
+    super(props)
+  }
   handleClick(e,key) {
+    console.log("The click functions");
     this.props.onHandleClick(e, key)
   }
+
   render() {
     let filteredMuseums = this.props.filteredMuseums;
     let museums = this.props.museums;
     return(
-      <div>
-      {filteredMuseums &&
-        <ul className="museums-list">{filteredMuseums.map(museum =>
-          <li
-            key={museum.id}
-            className="list-item"
-            tabIndex="0"
-            onClick={(e, key) => this.handleClick(e, museum.id)}
-          >{museum.name}
-          </li>)}}
-        </ul>
-      }
-      </div>
+
+      <nav key="drawer" className="nav">
+        {filteredMuseums &&
+          <ul className="nav-list">{filteredMuseums.map(museum =>
+            <li
+              id={museum.id}
+              key={museum.id}
+              className="list-item"
+              tabIndex={this.props.openMenu ? "-1" : "0"}
+              onClick={(e, key) => this.handleClick(e, museum.id)}
+            >{museum.name}
+            </li>)}
+          </ul>
+        }
+      </nav>
     );
   }
 }

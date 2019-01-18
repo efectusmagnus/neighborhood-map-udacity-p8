40b09585d63256e2d8cf14b260b1cb11c341436c
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2019-01-19 00:51:25 +0100

    Erase wrong code (!important)

--- src/js/MuseumsPopup.js
+++ src/js/MuseumsPopup.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import { InfoWindow } from 'react-google-maps';
+import React from 'react'
+import { InfoWindow } from 'react-google-maps'
 
 function MuseumsPopup(props) {
   let marker = props.marker;
   let image = marker.categories[0].icon.prefix;
   return(
-    <InfoWindow
-      //marker={this.state.activeMarker}
-      //visible={this.state.showingInfoWindow}
-      key={ marker.name }
-      className="info-window"
-      position={{lat: marker.location.lat, lng: marker.location.lng}}
-    >
+    <InfoWindow>
+      <div className="popup-decoration">
       <h3>{marker.name}</h3>
       <p>{marker.location.address}</p>
       <p>{marker.location.postalCode}</p>

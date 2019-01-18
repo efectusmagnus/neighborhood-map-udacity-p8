fe161a4080652c09a7a908cada39f3c4ac12726a
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2019-01-19 00:35:02 +0100

    Erase old colde (!important)

--- src/js/MuseumsMarker.js
+++ src/js/MuseumsMarker.js
@@ -1,54 +1,41 @@
-import React, { Component } from 'react';
-import { Marker, InfoWindow } from 'react-google-maps';
-import MuseumsPopup from './MuseumsPopup';
-import museumIcon from '../images/map-icon-32.png';
+import React, { Component } from 'react'
+import { Marker } from 'react-google-maps'
+import MuseumsPopup from './MuseumsPopup'
+import museumIcon from '../images/map-icon-32.png'
 
 class MuseumsMarker extends Component {
   constructor(props) {
     super(props)
     this.state = {
       showingInfoWindow: false,
-      activeMarker: {},
-      selectedPlace: {},
-      //infoWindowOpen: false,
       animation: '',
     }
     this.onMarkerClick = this.onMarkerClick.bind(this);
-    //this.handleClick = this.handleClick.bind(this)
   }
-  onMarkerClick = (props, marker, e) => {
+  onMarkerClick = (e, key) => {
+    const openInfoWindow = this.state.openInfoWindow
     this.setState({
-      selectedPlace: props,
-      //activeMarker: marker,
-      showingInfoWindow: true
+      showingInfoWindow: !showingInfoWindow
     });
   }
-  handleClick(e, key) {
-    let markerClick = e;
-    let infoWindowOpen = this.state.infoWindowOpen;
-    this.setState({
-      infoWindowOpen: !infoWindowOpen
-    })
-  }
+
   render() {
-    let infoWindowOpen = this.state.infoWindowOpen;
-    let animateMarker = this.props.animateMarker;
-    let animation = this.state.animation;
+    const openInfoWindow = this.state.openInfoWindow
+    const animateMarker = this.props.animateMarker
+    const animation = this.state.animation
     if (animateMarker && infoWindowOpen === false) {
       animateMarker.map(m => {
         if (m.id === this.props.id) {
           this.setState({
-            infoWindowOpen: !infoWindowOpen,
+            openInfoWindow: !openInfoWindow,
             animation: 4,
           })
         }
       })
 
     }
-    let marker = this.props.marker;
-    /*if (this.props.animateMarker.id === marker.id) {
+    const marker = this.props.marker;
 
-    }*/
     return(
       <Marker
         key={marker.title}
@@ -57,11 +44,11 @@ class MuseumsMarker extends Component {
         icon={museumIcon}
         position={{lat: marker.location.lat, lng: marker.location.lng}}
         //onClick={this.onMarkerClick}
-        onClick={(e, key) => this.handleClick(e, this.props.marker.id)}
+        onClick={(e, key) => this.onMarkerClick(e, this.props.marker.id)}
         animation={this.state.animation}
       >
         <React.Fragment>
-          {this.state.infoWindowOpen === true && (
+          {this.state.openInfoWindow === true && (
             <React.Fragment>{
               this.props.hasError === false && (
               <MuseumsPopup marker={marker} />)
@@ -72,73 +59,5 @@ class MuseumsMarker extends Component {
     )
   }
 }
-/*class MuseumsMarker extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      showingInfoWindow: false,
-      activeMarker: {},
-      selectedPlace: {},
-      //infoWindowOpen: false,
-      animation: '',
-    }
-    this.onMarkerClick = this.onMarkerClick.bind(this);
-    //this.handleClick = this.handleClick.bind(this)
-  }
-  onMarkerClick = (props, marker, e) => {
-    this.setState({
-      selectedPlace: props,
-      //activeMarker: marker,
-      showingInfoWindow: true
-    });
-  }
-  handleClick(e, key) {
-    let markerClick = e;
-    let infoWindowOpen = this.state.infoWindowOpen;
-    this.setState({
-      infoWindowOpen: !infoWindowOpen
-    })
-  }
-  render() {
-    let infoWindowOpen = this.state.infoWindowOpen;
-    let animateMarker = this.props.animateMarker;
-    let animation = this.state.animation;
-    if (animateMarker && infoWindowOpen === false) {
-      animateMarker.map(m => {
-        if (m.id === this.props.id) {
-          this.setState({
-            infoWindowOpen: !infoWindowOpen,
-            animation: 4,
-          })
-        }
-      })
 
-    }
-    let marker = this.props.marker;
-    if (this.props.animateMarker.id === marker.id) {
-
-    }
-    return(
-      <Marker
-        key={marker.title}
-        className="museum-icon"
-        title={marker.name}
-        icon={museumIcon}
-        position={{lat: marker.location.lat, lng: marker.location.lng}}
-        //onClick={this.onMarkerClick}
-        onClick={(e, key) => this.handleClick(e, this.props.marker.id)}
-        animation={this.state.animation}
-      >
-        <React.Fragment>
-          {this.state.infoWindowOpen === true && (
-            <React.Fragment>{
-              this.props.hasError === false && (
-              <MuseumsPopup marker={marker} />)
-            }</React.Fragment>
-          )}
-        </React.Fragment>
-      </Marker>
-    )
-  }
-}*/
 export default MuseumsMarker;

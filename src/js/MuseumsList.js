04bc9155268ecdd2ca808ea90748a74d0e03d0ba
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2019-01-19 01:24:10 +0100

    Erase old code and correct arrow function

--- src/js/MuseumsList.js
+++ src/js/MuseumsList.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react'
 
 class MuseumsList extends Component {
-  constructor(props) {
-    super(props)
-  }
+
   handleClick(e,key) {
-    console.log("The click functions")
     this.props.onHandleClick(e, key)
   }
 
   render() {
     let filteredMuseums = this.props.filteredMuseums
-    let museums = this.props.museums
     return(
-
       <nav key="drawer" className="nav">
         {filteredMuseums &&
-          <ul className="nav-list">{filteredMuseums.map(museum =>
+          <ul className="nav-list">{filteredMuseums.map(museum => (
             <li
               id={museum.id}
               key={museum.id}
@@ -24,7 +19,7 @@ class MuseumsList extends Component {
               tabIndex={this.props.openMenu ? "-1" : "0"}
               onClick={(e, key) => this.handleClick(e, museum.id)}
             >{museum.name}
-            </li>)}
+            </li>))}
           </ul>
         }
       </nav>

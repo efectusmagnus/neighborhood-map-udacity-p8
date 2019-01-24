26c4a897f1680733ec55792b978b6a1985b994eb
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2019-01-24 04:40:40 +0100

    Add hover effect to markers. Clean code.

--- src/js/MuseumsMarker.js
+++ src/js/MuseumsMarker.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { Marker } from 'react-google-maps'
 import MuseumsPopup from './MuseumsPopup'
-import museumIcon from '../images/map-icon-32.png'
+import museumsIcon from '../images/museums-icon_v8.png'
+import mouseOverIcon from '../images/museums-icon-mouse-over_v8.png'
 
 class MuseumsMarker extends Component {
   constructor(props) {
@@ -9,21 +10,40 @@ class MuseumsMarker extends Component {
     this.state = {
       openInfoWindow: false,
       animation: '',
+      imgSrc: museumsIcon
     }
+    //Bind `this` to event-handler functions
     this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMarkerMouseOver = this.onMarkerMouseOver.bind(this);
+    this.onMarkerMouseOut = this.onMarkerMouseOut.bind(this);
   }
-  onMarkerClick = (e, key) => {
+  // Open the InfoWindow with a click on the Marker
+  onMarkerClick() {
     const openInfoWindow = this.state.openInfoWindow
 
     this.setState({
       openInfoWindow: !openInfoWindow
     });
   }
+  // Inspiration of mouse over effect from:
+  // http://blog.sodhanalibrary.com/2016/07/change-image-source-on-mouse-hover.html#.XEjnD817ncs
+  // Show orange icon when mouse over
+  onMarkerMouseOver() {
+      this.setState({
+        imgSrc: mouseOverIcon
+      });
+  }
+  // Show black icon when mouse out
+  onMarkerMouseOut() {
+    this.setState({
+      imgSrc: museumsIcon
+    });
+  }
 
   render() {
     const openInfoWindow = this.state.openInfoWindow
     const animateMarker = this.props.animateMarker
-    
+
     if (animateMarker && openInfoWindow === false) {
       animateMarker.map(m => {
         if (m.id === this.props.id) {
@@ -33,7 +53,6 @@ class MuseumsMarker extends Component {
           })
         }
       })
-
     }
     const marker = this.props.marker;
 
@@ -42,9 +61,12 @@ class MuseumsMarker extends Component {
         key={marker.title}
         className="museum-icon"
         title={marker.name}
-        icon={museumIcon}
+        icon={this.state.imgSrc}
+        style={{'height': "20em"}}
         position={{lat: marker.location.lat, lng: marker.location.lng}}
-        onClick={(e, key) => this.onMarkerClick(e, this.props.marker.id)}
+        onClick={this.onMarkerClick}
+        onMouseOver={this.onMarkerMouseOver}
+        onMouseOut={this.onMarkerMouseOut}
         animation={this.state.animation}
       >
         <React.Fragment>

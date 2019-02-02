94c100aae7f42326c67a15258ee78c864d573ce9
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2019-02-02 13:36:02 +0100

    Correct keypress event of list items.

--- src/js/MuseumsList.js
+++ src/js/MuseumsList.js
@@ -5,7 +5,16 @@ class MuseumsList extends Component {
   listItemClick(e, key) {
     this.props.onListItemClick(e, key)
   }
-
+  // Source: "https://www.w3schools.com/jsref/event_key_keycode.asp"
+  listItemPressEnter(e, key) {
+    // Cancel the default action, if needed
+    e.preventDefault()
+    // Number 13 is the "Enter" key on the keyboard
+    if (e.which === 13 || e.keyCode === 13) {
+      // Trigger the button element with the enter key
+      this.props.onListItemClick(e, key)
+    }
+  }
   render() {
     let filteredMuseums = this.props.filteredMuseums
 
@@ -20,7 +29,7 @@ class MuseumsList extends Component {
               //role="menuitem"
               tabIndex="0"
               onClick={(e, key) => this.listItemClick(e, museum.id)}
-              onKeyPress={(e, key) => this.listItemClick(e, museum.id)}
+              onKeyPress={(e, key) => this.listItemPressEnter(e, museum.id)}
             >{museum.name}
             </li>))}
           </ul>

9780dde1f3f33dd4e901e1f57cd8cf7779c9b1c8
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2019-02-05 23:29:37 +0100

    Add redirection of page (react-router-dom)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 //import GoogleMapsContainer from './Test'
+import { BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const PageNotFound = ({location}) => (
+  <div>
+    <h3>No match for <code>{location.pathname}</code></h3>
+    <p>The page you were looking for was moved or does not exist.</p>
+  </div>
+)
+
+ReactDOM.render(
+  <BrowserRouter>
+  <Router>
+      <Switch>
+        <Route path="/" alt="home page" exact component={ App } />
+        <Redirect component={ PageNotFound } to="/" />
+      </Switch>
+    </Router>
+  </BrowserRouter>,
+  document.getElementById('root'));
 //ReactDOM.render(<GoogleMapsContainer />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
